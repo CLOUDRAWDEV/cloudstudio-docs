71c266d4a56c3bd4160ca789210ac3c7e3c0be0c
jylee <jylee@kismi.kr>
2022-08-26 10:41:43 +0900

    [chore] github workflow test

--- website/siteConfig.js
+++
@@ -1,6 +0,0 @@
-const siteConfig = {
-    url: "https://cloudrawdev.github.io",
-    baseUrl: "/cloudstudio-docs.cloudraw.kr/",
-    projectName: "cloudstudio-docs.cloudraw.kr",
-    organizationName: "cloudrawdev"
-}
