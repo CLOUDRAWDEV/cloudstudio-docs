099ffabaf1112e27943fb86415d7424393d94ae7
jylee <jylee@kismi.kr>
2022-08-26 09:43:19 +0900

    [chore] siteconfig파일 추가

---
+++ website/siteConfig.js
@@ -0,0 +1,6 @@
+const siteConfig = {
+    url: "https://cloudrawdev.github.io",
+    baseUrl: "/cloudstudio-docs.cloudraw.kr/",
+    projectName: "cloudstudio-docs.cloudraw.kr",
+    organizationName: "cloudrawdev"
+}
