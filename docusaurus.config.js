5d062c1099a61962f2baac5550da75453673e9e6
dheo <dheo@kismi.kr>
2022-09-27 00:49:43 +0000

    [feat] 부산 클라우드 엑스포 관련 공지사항 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Cloud Studio Docs",
+  title: "Cloud Studio Documents",
   tagline: "",
   // url: "https://cloudrawdev.github.io",
   url: "https://docs.cloudraw.kr",
@@ -102,7 +102,7 @@ plugins: [
             position: "left"
           },
           {
-            href: "https://join.slack.com/t/w1658372636-gh4700456/shared_invite/zt-1d0rtffdt-TzJ_8YqxPX5mPgWDY5SbKA",
+            href: "https://join.slack.com/t/cloudraw/shared_invite/zt-1gazpq5ky-lZQwzvjwmr25JazlrLgyhA",
             label: "Contact Slack",
             position: "left",
           },
