28d42b6ea7a10303c3165b47625baf1132c9b579
ckkim <ckkim@kismi.kr>
2025-05-07 09:30:57 +0900

    [chore] 개인정보 처리방침 업데이트

--- src/pages/policy/personal.tsx
+++ src/pages/policy/personal.tsx
@@ -3,12 +3,16 @@ import policyStyles from "@site/src/scss/_policy.module.scss"
 import Policy20240401 from "@site/src/components/Policies/20240401"
 import Policy20230201 from "@site/src/components/Policies/20230201"
 import Layout from "@theme/Layout"
+import Policy20250501 from "@site/src/components/Policies/20250501"
 
 export default function Personal() {
-    const [current, setCurrent] = useState("20240401")
+    const [current, setCurrent] = useState("20250501")
     const [policyComponent, setPolicyComponent] = useState(null)
     useEffect(() => {
         switch (current) {
+            case "20250501":
+                setPolicyComponent(<Policy20250501/>)
+                break
             case "20240401":
                 setPolicyComponent(<Policy20240401/>)
                 break
@@ -27,6 +31,7 @@ export default function Personal() {
                 <select onChange={(e)=>{
                     setCurrent(e.target.value)
                 }}>
+                    <option  value={"20250501"}>2025.05.01</option>
                     <option value={"20240401"}>2024.04.01</option>
                     <option  value={"20230201"}>2023.02.01</option>
                 </select>
